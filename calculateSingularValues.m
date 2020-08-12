53c7ce3a059b458bee6bb6bb03d711b2fac59f96
dsashid <57812008+dsashid@users.noreply.github.com>
2020-08-12 16:03:52 -0700

    Delete calculateSingularValues.m

--- calculateSingularValues.m
+++
@@ -1,8 +0,0 @@
-function [u,s,v] = calculateSingularValues(Xtotal)
-%CALCULATESINGULARVALUES: calculates singular values from mean subtracted data
-
-%Take SVD of whole train set
-[u,s,v]=svd(Xtotal-mean(Xtotal(:)),'econ');
-
-end
-
